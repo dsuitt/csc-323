738d602ae47ad681f1cd44e81822a49744f63813
doug suitt <dsuitt@globebiomedical.com>
2025-01-28 22:29:39 -0800

    cleaned up code from Monday.

--- squareTheNumber/index.js
+++ squareTheNumber/index.js
@@ -1,5 +1,5 @@
-const functions = require('@google-cloud/functions-framework');
+const functions = require('@google-cloud/functions-framework'); //the functions framework module is a wrapper around the express module that allows us to run our functions locally for testing. There are some good tutorials on how to use the functions framework on the Google Cloud Functions documentation page.
 
 functions.http('squareMyNumber', (req, res) => {
-  res.send(`Your number cubed is ${req.body.number**3}`);
+  res.send(`Your number squared is ${req.body.number**2}`);
 });
