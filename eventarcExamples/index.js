1d2c46b46128336e2626b7350530288d69a9b1c4
DESKTOP-K5KIVCS\Doug <dsuitt@globebiomedical.com>
2025-03-16 19:12:47 -0700

    eventarc example

---
+++ eventarcExamples/index.js
@@ -0,0 +1,15 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.cloudEvent('helloGCS', cloudEvent => {
+  console.log('Event received: ', cloudEvent)
+  console.log(`Event ID: ${cloudEvent.id}`);
+  console.log(`Event Type: ${cloudEvent.type}`);
+
+  const file = cloudEvent.data;
+  console.log('Cloud Event data: ', file);
+  console.log(`Bucket: ${file.bucket}`);
+  console.log(`File: ${file.name}`);
+  console.log(`Metageneration: ${file.metageneration}`);
+  console.log(`Created: ${file.timeCreated}`);
+  console.log(`Updated: ${file.updated}`);
+});
